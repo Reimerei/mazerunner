a43d4cf1e9620951a6c1614bdf27b066c3d75095
Björn Reimer <git@bjrm.de>
2015-10-30 14:13:41 +0100

    created intellij project and added warwickmaze

---
+++ src/MazeRunner.java
@@ -0,0 +1,29 @@
+import uk.ac.warwick.dcs.maze.logic.IRobot;
+
+public class MazeRunner {
+
+    /*
+        robot.advance();
+
+        robot.face(IRobot.AHEAD);
+        robot.face(IRobot.RIGHT);
+        robot.face(IRobot.LEFT);
+        robot.face(IRobot.BEHIND);
+
+        robot.look(IRobot.AHEAD);
+        robot.look(IRobot.LEFT);
+        robot.look(IRobot.RIGHT);
+        robot.look(IRobot.BEHIND);
+
+        IRobot.WALL;
+        IRobot.PASSAGE;
+        IRobot.BEENBEFORE;
+    */
+
+    public void controlRobot(IRobot robot) {
+
+
+
+    }
+
+}
